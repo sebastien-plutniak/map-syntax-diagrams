a8f3459464e3dc98e0b595dd46b403b976525556
Seb <seb.transmission@riseup.net>
2020-03-26 19:36:07 +0100

    version 2

--- app/app.R
+++ app/app.R
@@ -1,28 +1,43 @@
 library(shiny)
+library(ggplot2)
 library(shinythemes)
 
-# Define UI ----
+# DEFINE UI ----
 ui <- fluidPage(
   theme = shinytheme("slate"),
-  
-  titlePanel("MAP syntax diagrams"),
-  uiOutput("map"),
-  uiOutput("author"),
+  # titlePanel("MAP syntax diagrams"),
+  br(),
+  a(href="https://map-polytheisms.huma-num.fr", 
+    img(src='map-logo.png', width="250", align = "left")),
+  br(), br(),  br(), 
+  h3("MAP syntax diagrams"),
+  p("A research tool by",
+    a("S. Plutniak", href="https://sebastien-plutniak.github.io")),
   br(),
   
   sidebarLayout(
     sidebarPanel(
-      fileInput('file', 'Choose CSV file',
+      fileInput('file', 'Choose CSV file:',
                 accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+      radioButtons(inputId = 'sep', label = 'Separator:', 
+                   choices = c("," =',' , ";"=';'
+                               ,"tab"='\t'), inline=T, selected = ','),
+      
       checkboxInput("use_example", "use example data"),
       numericInput("inputSeqlen",
-                  "Formula max. length", 
+                  "Sequence max. length:", 
                  value = 8, min = 2, max = 30),
       conditionalPanel(condition = "typeof output.stats !== 'undefined'",
-        downloadButton("downloadData", "Download stats"),
+        downloadButton("downloadData", "Statistics table"),
         br(),br()
         ),
+      conditionalPanel(condition = "typeof output.plot !== 'undefined'",
+                       downloadButton("downloadPlot", "Plot (svg)"),
+                       br(),br()
+      ),
       uiOutput("var"),
+      conditionalPanel(condition = "output.twoGraphs",
+                       checkboxInput("graphDiff", "Show differences", value = F)),
       uiOutput("values"),
       width = 2
     ),
@@ -30,57 +45,48 @@ ui <- fluidPage(
     mainPanel(
       column(12, align="center",
              h2("Input table"),
-             br(),
              tableOutput("tab"),
-             h2("Graph statistics"),
+             br(),
+             h2("Sequence lengths"),
+             imageOutput("seqlength", width = "90%", height = "250px"),
+             br(),
+             h2("Diagram statistics"),
              tableOutput("stats"),
              br(),
              h2("Plot"),
-                plotOutput("plot", width= "100%" )
+             imageOutput("plot", width= "100%" )
               ),
       width = 10)
 ))
-    
-# Define server logic ----
-server <- function(input, output) {
-  
-  output$author <- renderUI({ tagList("by",
-                                      a("S. Plutniak", href="https://sebastien-plutniak.github.io")) })
-  output$map <- renderUI({ tagList("A research tool for the “",
-               a("Mapping Ancient Polytheisms", href="https://map-polytheisms.huma-num.fr/"),
-               "” project"
-               ) })
+
+# DEFINE SERVER  ----    
+server <- function(input, output) { 
   
   source("syntax-diagram.R")
   
-  # data <- reactive({
-  #   # req(input$file)
-  #       read.csv(input$file$datapath, header=T, stringsAsFactors = F)
-  # })
-  
   userFile <- reactive({
-    # If no file is selected, don't do anything
+    # attente du fichier de l'utilisateur ----
     validate(need(input$file, message = ""))
     input$file
   })
   
-  # The user's data, parsed into a data frame
+  # récupération du tableau de l'utilisateur ----
   data <- reactive({
         if(input$use_example == F){
-          read.csv(userFile()$datapath, header=T, stringsAsFactors = F)
+          # read.csv(userFile()$datapath, header=T, stringsAsFactors = F)
+          read.csv(userFile()$datapath, header=T,
+                   sep=input$sep, stringsAsFactors = F)
         }else{
           read.csv("sample.csv", header=T, stringsAsFactors = F)
         }
   })
   
-  output$tab <- renderTable({
-    # req(input$file)
+  output$tab <- renderTable({ # aperçu du tableau de l'utilisateur ----
     data <- data()
     head(data, 3)
     })
 
   output$var <- renderUI({
-     # req(input$file)
      data <- data()
      items <- names(data)
      names(items) <- items
@@ -94,67 +100,120 @@ server <- function(input, output) {
       names(val) <- val
       checkboxGroupInput("values", "Values:", c("All", val) )
     })
-
-    output$plot <- renderPlot({  # plot ----
+    
+    output$twoGraphs <- renderText({ # gestion du graphe des différences ----
+      if(length(input$values) == 2)
+         "true"
+      })
+    outputOptions(output, "twoGraphs", suspendWhenHidden = FALSE) # nécessaire
+    
+    plotInput <- function(){ # fonction générale pour le plot des diagrammes----
       req(input$values)
       
       data <- data()
       data <- make_map_formula(data)
       seq  <- make_map_sequence(data)
 
-      # sélection des longueurs maximales de séquences :
-      selection.len <- seqlength(seq) <= input$inputSeqlen + 1
+      # calcul des longueurs des séquences ----
+      seq.list.full <- lapply(input$values[input$values != "All"],
+      # seq.list.full <- lapply(input$values,
+                              function(sel){
+                                # sélection variable %in% values :
+                                selection.v <- eval(parse(text = paste0("data$", input$var))) %in% sel
+                                seq[ selection.v, ]
+                              })
+      names(seq.list.full) <- input$values[input$values != "All"]
+      # si requis, ajout de l'ensemble du corpus :
+      if("All" %in% input$values)                        {
+        seq.list.full$"All" <- seq
+      }
+      lengths.df <- lapply(1:length(seq.list.full), function(x)
+        data.frame(subset = names(seq.list.full)[x],
+                   Length = seqlength(seq.list.full[[x]]) ) )
+      lengths.df <- do.call("rbind", lengths.df)
+      # plot des longueurs des séquences  :
+      output$seqlength <- renderPlot({
+        ggplot(lengths.df, aes(x = Length, color = subset, group = subset) ) +
+          theme_linedraw(base_size = 14) +
+          geom_path(stat="bin", bins = max(lengths.df$Length) - 1, size= .8) +
+          scale_color_viridis_d()
+      })
       
-      seq.list <- lapply(input$values, function(sel){
+      # sélection des sous-ensembles de séquences à étudier ----
+      #  longueurs maximales de séquences : 
+      selection.len <- seqlength(seq) <= input$inputSeqlen + 1
+      # stockage des sous-ensembles dans une liste :
+      seq.list <- lapply(input$values[input$values != "All"],
+        function(sel){
         # sélection variable %in% values :
-        selection.v <- eval(parse(text = paste0("data$", input$var))) %in% sel
-        # union des deux sélections :
-          sel <- selection.v & selection.len
-          seq[ sel, ]
+          selection.v <- eval(parse(text = paste0("data$", input$var))) %in% sel
+          # union des deux sélections :
+          sel.join <- selection.v & selection.len
+          seq[ sel.join, ]
         })
       
-      names(seq.list) <- input$values
-      
+      names(seq.list) <- input$values[input$values != "All"]
+      #  si requis, ajout du diagramme de l'ensemble des séquences: ----
       if("All" %in% input$values ){
-        seq.list$"All" <- seq[selection.len, ]
+        seq.list$"All" <- seq[ selection.len, ]
       }
       
       # stats sur les données ----
       data.stats <- make_data_stats(seq.list)
       
+      # calcul des taux de transition, conversion en graphe :
       seq.list <- lapply(seq.list, function(x){ seqtrate(x, time.varying=1) })
       g.list <- lapply(seq.list, function(x){ lapply(1:input$inputSeqlen,
                                function(xx){ map_seq_graph(x, from.level=xx)})})
       g.list <- lapply(g.list, function(x){ Reduce(join_map_graphs, x)})
-
-      # stats sur les graphes et tableau en sortie----    
+      
+      # stats sur les graphes et tableau à télécharger  ----    
       output$stats <- renderTable({
-      stats <- rbind(data.stats,
-                     map_seq_stats(g.list))
-
-      # downloadable csv of selected dataset
-      thedata <- reactive(stats)
-      output$downloadData <- downloadHandler(
+      stats <- rbind(data.stats,  map_seq_stats(g.list))
+      
+      output$downloadData <- downloadHandler( 
         filename = function(){"stats.csv"},
         content = function(fname){
-          write.csv(thedata(), fname)
+          write.csv(stats, fname)
         }
       )
-        stats
-      }, rownames = T )
+      stats
+      }, rownames = T )  
+      
+      # si requis, ajout du diagramme des différences : ----
+      if(input$graphDiff){ 
+        g.list$"Difference" <- map_seq_diff(g.list[[1]], g.list[[2]])
+      }
       
       # plot function ----
-      par(mfrow = c(ceiling(length(input$values) / 2), 2 ) )
+      par(mfrow = c(ceiling( length(g.list) / 2), 2 ) )
       for(i in 1:length(g.list)){
           map_seq_plot(g.list[[i]],
-                       main = input$values[i])
+                       type = g.list[[i]]$type,
+                       main = names(g.list)[i] )
       }
-        },
-        height = function(){1 + ceiling(length(input$values) / 2) * 600},
+   }
+      
+    output$plot <- renderPlot({  # plot des diagramme ----
+      
+      list(src = plotInput(),
+           height = function(){1 + ceiling( c(length(input$values ) + sum(input$graphDiff)) / 2) * 1000},
+           alt = "This is alternate text")
+    },
+        height = function(){1 + ceiling( c(length(input$values ) + sum(input$graphDiff)) / 2) * 600},
         res = 100
-      # )
-
-  )
+    )
+    
+    # téléchargement de l'image en svg ---- 
+      output$downloadPlot <- downloadHandler(
+        filename = "syntax-diagram.svg",
+        content = function(file) {
+          svg(file, width = 10, pointsize = 9,
+              height =  c(1 + ceiling( c(length(input$values) + sum(input$graphDiff))  / 2)   * 5 ) )
+           plotInput()
+          dev.off()
+        }
+        )
 }
 
 # Run the app ----
--- app/syntax-diagram.R
+++ app/syntax-diagram.R
@@ -6,6 +6,7 @@ library(plyr) # pour rbind.fill
 library(xtable)
 
 
+# fct ajoutant les formules reformatées:
 make_map_formula <- function(df){
   # sélection de la première formule s'il en existe plusieurs versions:
   df$formule.splited <- unlist(lapply(df$formule,
@@ -28,6 +29,7 @@ make_map_formula <- function(df){
   df
 }
 
+# Fct générant des objets séquences à partir des formules en chaînes de caractères:
 make_map_sequence <- function(df){
   # transformer en data.frame:
   seq.df <- lapply(df$formule.splited,
@@ -42,7 +44,7 @@ make_map_sequence <- function(df){
   seq
 }
 
-
+# Fct transformant la matrice de transition en graphe:
 map_seq_graph <- function(trans.matrix, from.level){
   elist <- trans.matrix[,, from.level]
   elist <- melt(elist, value.name = "weight")
@@ -58,6 +60,7 @@ map_seq_graph <- function(trans.matrix, from.level){
   g
 }
 
+# Fct fussionnant deux graphes en conservant les attributs:
 join_map_graphs <- function(g1, g2){
   g <- graph.union(g1, g2)
   if(is.null(E(g)$weight_1)) return(g)
@@ -66,37 +69,89 @@ join_map_graphs <- function(g1, g2){
   g <- delete_edge_attr(g, "weight_1")
   g <- delete_edge_attr(g, "weight_2")
   V(g)$name2 <- gsub("[0-9]", "",  V(g)$name)
+  g$type <- "normal"
   g
 }
 
-map_seq_plot <- function(g, main=""){
+# Fct générant une la sortie graphique d'un ou plusieurs graphes:
+map_seq_plot <- function(g, type="normal", main=""){
+  # préparation des coordonnées des sommets :
+  g$layout <- layout_as_tree(g, root = V(g)[1])
+  g$layout <- geomorph::rotate.coords(g$layout, type= "rotateCC")
+  g$layout <- norm_coords(g$layout)
+  
+  if( g$type == "difference" ){ # pour les diagrammes de différence:
+    # préparation des attributs :
+    V(g)$name2 <- gsub("[0-9]", "",  V(g)$name)
+    # plot du graphe :
+    plot(g, 
+         vertex.size=10, 
+         vertex.color = V(g)$color,
+         vertex.label = V(g)$name2,
+         layout =  g$layout,
+         edge.arrow.size=.2,edge.arrow.width = .8,
+         edge.width = 2,
+         edge.color = E(g)$color,
+         main = "Differences")
+    text(-1, -.9, paste("≠ nodes =", g$v.diff), pos=4, cex=.8, col="red") 
+    text(-1, -1, paste("≠ edges =", g$e.diff), pos=4, cex=.8, col="red") 
+  }
+  else{ # pour les diagrammes normaux :
+    # préparation des attributs :
+    V(g)$color <- "white"
+    V(g)[degree(g, mode= "out") == 0]$color <- "grey90"
+    # plot du graphe :
+    plot(g, 
+         vertex.size=10, 
+         vertex.color = V(g)$color,
+         vertex.label = V(g)$name2,
+         layout =  g$layout,
+         edge.arrow.size=.3,  edge.arrow.width = 1,
+         edge.width = E(g)$weight * 3 + 1,
+         edge.label = round(E(g)$weight, 2),
+         edge.color = as.character(factor(E(g)$weight,
+                                          levels = sort(unique(E(g)$weight)),
+                                          labels = rainbow(length(unique(E(g)$weight)),
+                                                           start=0, end = .15, alpha=.9, rev=T))),
+         edge.label.cex=.7, edge.label.color= "black",
+         main = main)
+  }
+  # ajout habillage :
+  axis(1, unique(g$layout[,1]), 
+       0:c(length(unique(g$layout[,1]))-1))
+  title(xlab = "Symbols ranks")
+}
+
+# fct renvoyant un graphe des différences entre deux graphes:
+map_seq_diff <- function(g1, g2){
+  # calcul de la différence et de l'union des deux graphes:
+  g  <- graph.union(g1, g2)
+  g.diff <-  graph.union(difference(g1, g2),
+                         difference(g2, g1))
+  g.diff <- delete_vertices(g.diff, degree(g.diff, mode="all")==0)
+  # préparation des attributs du graphe final:
+  E(g)$weight <- 1
+  V(g)$color <- "white"
+  E(g)$color <- "grey"
+  # recherche des arêtes et sommets communs :
+  sel.e <- apply(get.edgelist(g), 1, paste, collapse="") %in%
+    apply(get.edgelist(g.diff), 1, paste, collapse="")
+  sel.v <- ( V(g)$name %in% V(g1)$name ) & ( V(g)$name %in% V(g2)$name ) 
+  # marquage des différences:
+  E(g)[ sel.e ]$color <- "red"
+  V(g)[ ! sel.v ]$color <- "red"
   # préparation des coordonnées des noeuds :
   g$layout <- layout_as_tree(g, root = V(g)[1])
   g$layout <- geomorph::rotate.coords(g$layout, type= "rotateCC")
   g$layout <- norm_coords(g$layout)
-  V(g)$color <- "white"
-  V(g)[degree(g, mode= "out") == 0]$color <- "grey90"
-  # plot du graphe :
-  plot(g, 
-       vertex.size=10, 
-       vertex.color = V(g)$color,
-       vertex.label = V(g)$name2,
-       layout =  g$layout,
-       edge.arrow.size=.3,  edge.arrow.width = 1,
-       edge.width = E(g)$weight * 3 + 1,
-       edge.label = round(E(g)$weight, 2),
-       edge.color = as.character(factor(E(g)$weight,
-                                        levels = sort(unique(E(g)$weight)),
-                                        labels = rainbow(length(unique(E(g)$weight)),
-                                                         start=0, end = .15, alpha=.9, rev=T))),
-       edge.label.cex=.6, edge.label.color= "black",
-       main = main)
-  # ajout habillage :
-  axis(1, unique(g$layout[,1]), 
-       0:c(length(unique(g$layout[,1]))-1))
-  title(xlab = "Rangs des symboles dans les séquences")
+  V(g)$name2 <- gsub("[0-9]", "",  V(g)$name)
+  g$type <- "difference"
+  g$e.diff <- length(which(sel.e))  # nombre d'arêtes différentes
+  g$v.diff <- length(which(! sel.v))  # nombre de sommets différentes
+  g
 }
 
+# fct renvoyant la moyenne des pondérations des arêtes des plus courts chemins:
 .shortest.path.weight.mean <-  function(g){
   edges <- shortest_paths(g, from = V(g)[1],
                           to = V(g)[degree(g, mode= "out") == 0],
@@ -105,6 +160,7 @@ map_seq_plot <- function(g, main=""){
   round(min(sapply(res, mean)), 2)
 }
 
+# fct renvoyant la valeur moyenne des pondération sur le diamètre
 .diameter.weight.mean <-  function(g){
   len.diameter <- length(get_diameter(g, weights = rep(1, ecount(g)))) - 1
   res <- distances(g, v = V(g)[1],
@@ -114,27 +170,32 @@ map_seq_plot <- function(g, main=""){
   round(max(res), 2)
 }
 
+# fct prenant une liste d'objets séquence et renvoyant un df de statistiques;
 make_data_stats <- function(seq.list){
   rbind(
-    "n. formules"   = sapply(seq.list, nrow),
-    "n. formules ≠" = sapply(seq.list, function(seq)
+    "n. sequences"   = sapply(seq.list, nrow),
+    "n. different sequences" = sapply(seq.list, function(seq)
                         length(which( ! duplicated(seq)))),
-    "longueur max." = sapply(seq.list, function(seq) max(seqlength(seq)) -1)
+    "sequence max. length" = sapply(seq.list, function(seq) max(seqlength(seq)) -1)
   )
 }
 
+# fct prenant une liste de graphes et renvoyant un df de statistiques (ou une sortie LaTeX):
 map_seq_stats <- function(g.list, xtable=F){
+  # sélection des graphes normaux: 
+  g.list <- g.list[ sapply(g.list, function(g) g$type == "normal") ]
+  
   trans.stats <- rbind(
-    "n. sommets" = sapply(g.list, gorder),
-    "n. symboles ≠" = sapply(g.list, function(x) length(unique(V(x)$name2)) - 1),
-    "n. symb. terminaux" = sapply(g.list, function(x) length(which(degree(x, mode="out")==0))),
-    "n. pts d'articulation" = sapply(g.list,
+    "n. nodes" = sapply(g.list, gorder),
+    "n. different symbols" = sapply(g.list, function(x) length(unique(V(x)$name2)) - 1),
+    "n. terminal symbols" = sapply(g.list, function(x) length(which(degree(x, mode="out")==0))),
+    "n. articulation points" = sapply(g.list,
                                      function(x) length(articulation_points(x))),
-    "centralisation de deg." = sapply(g.list,
+    "degree centralisation" = sapply(g.list,
                                       function(x) round(centr_degree(x)[[2]], 2)),
-    "prob. médiane" = sapply(g.list, function(x) round(median(E(x)$weight), 2)),
-    "prob. moy. diam." = sapply(g.list, function(x) .diameter.weight.mean(x)),
-    "prob. moy. + court ch." =  sapply(g.list, .shortest.path.weight.mean)
+    "median probability" = sapply(g.list, function(x) round(median(E(x)$weight), 2)),
+    "diameter mean prob." = sapply(g.list, function(x) .diameter.weight.mean(x)),
+    "shorter paths mean prob." =  sapply(g.list, .shortest.path.weight.mean)
     # c.bet = sapply(g.list, function(x) round(centr_betw(x)[[2]], 2))
   )
   if(xtable){
